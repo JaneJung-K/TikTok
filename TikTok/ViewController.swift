520978dd26d3f9a75ba77fc5b8a73460de1ad37f
JaneJung-K <piaojane@daum.net>
2021-09-16 10:34:25 +0900

    Setting up project

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by 정혜영 on 2021/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
