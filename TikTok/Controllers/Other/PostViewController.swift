dd7c57ccfbad744b51cd40f1386b684aee75488d
JaneJung-K <piaojane@daum.net>
2021-09-19 21:40:22 +0900

    동영상 종료시 다시 처음부터 재생 옵저버 추가

--- TikTok/Controllers/Other/PostViewController.swift
+++ TikTok/Controllers/Other/PostViewController.swift
@@ -63,6 +63,8 @@ class PostViewController: UIViewController {
     
     var player: AVPlayer?
     
+    private var playerDidFinishObserver: NSObjectProtocol?
+    
     // MARK: - Init
     
     init(model: PostModel) {
@@ -132,6 +134,20 @@ class PostViewController: UIViewController {
         view.layer.addSublayer(playerLayer)
         player?.volume = 0
         player?.play()
+        
+        guard let player = player else {
+            return
+        }
+        
+        playerDidFinishObserver = NotificationCenter.default.addObserver(
+            forName: .AVPlayerItemDidPlayToEndTime,
+            object: player.currentItem,
+            queue: .main
+        ){ _ in
+            player.seek(to: .zero)
+            player.play()
+        }
+        
     }
     
     @objc func didTapProfileButton() {
